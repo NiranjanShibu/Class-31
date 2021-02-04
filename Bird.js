4a99fba1fb4ff3f776f1bc4d8afb47966b53a6f0
NiranjanShibu <74334499+NiranjanShibu@users.noreply.github.com>
2021-02-03 19:46:50 -0600

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,27 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajoctry = [];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+
+      var pos = [this.body.position.x, this.body.position.y];
+      this.trajoctry.push(pos);
+      for(var i = 0; i < this.trajoctry.length; i++){
+  
+        image(this.smokeImage, this.trajoctry[i][0], this.trajoctry[i][1]);
+  
+      }
+
+    }
+    
+  }
+}
